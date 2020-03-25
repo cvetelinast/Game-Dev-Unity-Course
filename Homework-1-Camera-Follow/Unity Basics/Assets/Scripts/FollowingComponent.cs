b2db51b7ea301b24ea273e92b0bd267eada9d330
Cvetelina Stoyanova <cvetelinast.96@abv.bg>
2020-03-26 00:30:58 +0200

    Initial commit containing Homework 1

---
+++ Homework-1-Camera-Follow/Unity Basics/Assets/Scripts/FollowingComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingComponent : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 offset = new Vector3(-8, 5, -8);
+
+    [SerializeField]
+    private float smoothSpeed = 1.25f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var bodyTransform = GameObject.Find("Body").transform;
+        Vector3 desiredPosition = bodyTransform.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(bodyTransform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+        transform.LookAt(bodyTransform.position);
+    }
+}
