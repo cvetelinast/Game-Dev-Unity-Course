9f7bd095969986ff90451c1dcd3ff061b85363d3
Cvetelina Stoyanova <cvetelinast.96@abv.bg>
2020-03-27 23:56:05 +0200

    Adds jump on pressed the key "space"

--- Homework-2-Ground-Detection/Physics/Assets/Scripts/PlayerMovement.cs
+++ Homework-2-Ground-Detection/Physics/Assets/Scripts/PlayerMovement.cs
@@ -1,24 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	[SerializeField]
-	private float speed = 3;
+    [SerializeField]
+    private float speed = 3;
 
-	void Update() {
-		Vector3 moveDirection =
-				new Vector3(-Input.GetAxis("Vertical"),
-							+0,
-							+Input.GetAxis("Horizontal"))
-							.normalized
-				* Time.deltaTime
-				* speed;
-		Vector3 pointToLookAt = transform.position
-							  + moveDirection * 100;
+    [SerializeField]
+    private float jumpForce = 500;
 
-		transform.position += moveDirection;
-		transform.LookAt(pointToLookAt);
-	}
+    private Vector3 previousVelocity = Vector3.zero;
+
+    void Update() {
+        MoveHorizontally();
+        JumpIfPossible();
+    }
+
+    private void MoveHorizontally() {
+        Vector3 moveDirection =
+                new Vector3(-Input.GetAxis("Vertical"),
+                            +0,
+                            +Input.GetAxis("Horizontal"))
+                            .normalized
+                * Time.deltaTime
+                * speed;
+        Vector3 pointToLookAt = transform.position
+                              + moveDirection * 100;
+
+        transform.position += moveDirection;
+        transform.LookAt(pointToLookAt);
+    }
+
+    private void JumpIfPossible() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            var rigidbody = GetComponent<Rigidbody>();
+
+            var acceleration = (rigidbody.velocity - previousVelocity) / Time.fixedDeltaTime;
+            previousVelocity = rigidbody.velocity;
+
+            if (IsJumpAllowed(acceleration)) {
+                rigidbody.velocity = Vector3.up * jumpForce;
+            }
+        }
+    }
+
+    private bool IsJumpAllowed(Vector3 acceleration) {
+        return AlmostEqualsZero(acceleration);
+    }
+
+    private bool AlmostEqualsZero(Vector3 first) {
+        return AlmostEquals(first, Vector3.zero);
+    }
+
+    private bool AlmostEquals(Vector3 first, Vector3 second) {
+        return Mathf.Round(first.x) == Mathf.Round(second.x)
+        && Mathf.Round(first.y) == Mathf.Round(second.y)
+        && Mathf.Round(first.z) == Mathf.Round(second.z);
+    }
 }
