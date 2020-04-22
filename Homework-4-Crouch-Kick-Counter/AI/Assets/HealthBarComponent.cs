738b8bb4181c7f50fb1c2318f9ef316cce629ee4
Cvetelina Stoyanova <cvetelinast.96@abv.bg>
2020-04-23 01:48:48 +0300

    adds initial health bar

---
+++ Homework-4-Crouch-Kick-Counter/AI/Assets/HealthBarComponent.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarComponent : MonoBehaviour {
+    [SerializeField]
+    private Slider slider;
+    [SerializeField]
+    private Gradient gradient;
+    [SerializeField]
+    private Image fill;
+    public void SetMaxHealth(float health) {
+        slider.maxValue = health;
+        slider.value = health;
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetHealth(float health) {
+        slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+
+
+    private void Start() {
+        // SetMaxHealth(100f);
+    }
+
+    void Update() {
+        // SetHealth(slider.value - 1);
+    }
+}
--- Homework-4-Crouch-Kick-Counter/AI/Assets/Scripts/Health.cs
+++ Homework-4-Crouch-Kick-Counter/AI/Assets/Scripts/Health.cs
@@ -3,40 +3,45 @@ using static UnityEngine.Mathf;
 
 public class Health : MonoBehaviour {
 
-	[SerializeField]
-	private int health = 100;
+    [SerializeField]
+    private int health = 100;
 
-	private Animator animator;
-	public GameObject cross;
+    private Animator animator;
+    public GameObject cross;
+    [SerializeField]
+    private GameObject healthBar;
 
-	void Start() {
-		animator = GetComponent<Animator>();
-	}
+    void Start() {
+        animator = GetComponent<Animator>();
+        healthBar.GetComponent<HealthBarComponent>().SetMaxHealth(health);
+    }
 
-	public void SpawnCross() {
-		Vector2 spawnPosition = new Vector2 {
-			x = transform.position.x,
-			y = -0.1f
-		};
-		Instantiate(cross, spawnPosition, Quaternion.identity);
-	}
+    public void SpawnCross() {
+        Vector2 spawnPosition = new Vector2
+        {
+            x = transform.position.x,
+            y = -0.1f
+        };
+        Instantiate(cross, spawnPosition, Quaternion.identity);
+    }
 
-	public void Die() {
-		Destroy(gameObject);
-	}
+    public void Die() {
+        Destroy(gameObject);
+    }
 
-	public void TakeDamage() {
-		int damage = 10;
-		health = Max(health - damage, 0);
-		animator.SetInteger("Health", health);
-		animator.SetTrigger("TookDamage");
-	}
+    public void TakeDamage() {
+        int damage = 10;
+        health = Max(health - damage, 0);
+        healthBar.GetComponent<HealthBarComponent>().SetHealth(health);
+        animator.SetInteger("Health", health);
+        animator.SetTrigger("TookDamage");
+    }
 
-	private void OnTriggerEnter2D(Collider2D collision) {
-		if (collision.transform.parent != transform
-			&& collision.gameObject.CompareTag("Hitbox")) {
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.transform.parent != transform
+            && collision.gameObject.CompareTag("Hitbox")) {
 
-			TakeDamage();
-		}
-	}
+            TakeDamage();
+        }
+    }
 }
