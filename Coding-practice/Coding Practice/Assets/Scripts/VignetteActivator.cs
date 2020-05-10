2e1e49a454073ca510c7e7f94ee2522d6b82821b
Cvetelina Stoyanova <cvetelinast.96@abv.bg>
2020-05-11 02:05:15 +0300

    Added vignette effect handling

---
+++ Coding-practice/Coding Practice/Assets/Scripts/VignetteActivator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class VignetteActivator : MonoBehaviour {
+    [SerializeField] private DamageHandler damageHandler;
+
+    private void OnEnable() {
+        damageHandler.OnHealthChanged += changeHealth;
+    }
+
+    private void OnDisable() {
+        damageHandler.OnHealthChanged -= changeHealth;
+    }
+
+    private void changeHealth(int health) {
+        var effect = GetComponent<PostProcessingCameraEffect>();
+        if (health <= 20 && !effect.enabled) {
+            effect.enabled = true;
+        } else if (health > 20 && effect.enabled) {
+            effect.enabled = false;
+        }
+    }
+}
